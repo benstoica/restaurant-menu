78bbaebdbd38510781cc648472948aed01b4e8a5
Ben Stoica <bstoica@uwm.com>
2022-01-06 15:21:28 -0500

    refactor and removed unneeded file

--- app.js
+++ app.js
@@ -86,41 +86,7 @@ const container = document.querySelector(".btn-container");
 
 window.addEventListener("DOMContentLoaded", () => {
   displayMenuItems(menu);
-
-  const categories = menu.reduce(
-    (values, item) => {
-      if (!values.includes(item.category)) {
-        values.push(item.category);
-      }
-      return values;
-    },
-    ["all"]
-  );
-  const categoryBtns = categories
-    .map((category) => {
-      return `
-    <button type="button" class="filter-btn" data-id="${category}">
-    ${category}
-  </button>`;
-    })
-    .join("");
-  container.innerHTML = categoryBtns;
-  const filterBtns = document.querySelectorAll(".filter-btn");
-  filterBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const category = e.currentTarget.dataset.id;
-      const menuCategory = menu.filter((menuItem) => {
-        if (menuItem.category === category) {
-          return menuItem;
-        }
-      });
-      if (category === "all") {
-        displayMenuItems(menu);
-      } else {
-        displayMenuItems(menuCategory);
-      }
-    });
-  });
+  displayMenuButtons();
 });
 
 const displayMenuItems = (menuItems) => {
@@ -142,4 +108,41 @@ const displayMenuItems = (menuItems) => {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
+};
+
+const displayMenuButtons = () => {
+  const categories = menu.reduce(
+    (values, item) => {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map((category) => {
+      return `
+        <button type="button" class="filter-btn" data-id="${category}">
+        ${category}
+      </button>`;
+    })
+    .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter((menuItem) => {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 };
